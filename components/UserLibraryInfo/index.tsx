5766ed63863a870f8d45193ded83808fd225ac59
gabrielduumont <gabrielduumont@gmail.com>
2024-08-24 18:01:34 -0300

    Added song alpha sorting to main screen

--- components/UserLibraryInfo/index.tsx
+++ components/UserLibraryInfo/index.tsx
@@ -11,18 +11,23 @@ type SearchOption = GenericTypeWithId<{
 type UserLibraryInfoProps = {
   totalSongs?: number;
   searchOptions?: SearchOption[];
+  onToggleAlphabeticalSort?: (shouldSort: boolean) => void;
 };
 
 export default function UserLibraryInfo({
   totalSongs,
   searchOptions,
+  onToggleAlphabeticalSort,
 }: UserLibraryInfoProps) {
   const options = searchOptions && searchOptions.length ? searchOptions : [];
   return (
     <div className={styles.container}>
       <LibraryDescription totalSongs={totalSongs} />
       <div className={styles['sort-and-search']}>
-        <ToggleButton label="Sort from A-Z" />
+        <ToggleButton
+          label="Sort from A-Z"
+          onClick={onToggleAlphabeticalSort}
+        />
         <SearchInput
           showIcon
           label="Search in your library"
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,47 @@ import Layout from '@app/components/Layout';
 import PageContent from '@app/components/PageContent';
 import SongsList from '@app/components/SongsList';
 import UserLibraryInfo from '@app/components/UserLibraryInfo';
+import { Song } from '@app/data-types/song';
+import lodash from 'lodash';
 
 import JSONgs from '@app/server-payload.json';
+import { useEffect, useState } from 'react';
+
+const getSearchOptionsFromSongs = (songs: Song[]) => {
+  if (!songs || !songs.length) {
+    return [];
+  }
+
+  return songs.map((songEntry) => ({
+    id: `${songEntry.id}`,
+    value: songEntry.song.title,
+  }));
+};
 
 export default function HomePage() {
-  const songs = JSONgs.songs;
+  const [songs, setSongs] = useState<Song[]>(JSONgs.songs);
+  const [originalSongs] = useState<Song[]>(JSONgs.songs);
+
+  const onSortSong = (shouldSortAlphabetically: boolean) => {
+    if (shouldSortAlphabetically) {
+      const sortedSongs = lodash.orderBy(
+        songs,
+        (song) => song.song.title.toLowerCase(),
+        ['asc']
+      );
+      setSongs(sortedSongs);
+    } else {
+      setSongs(originalSongs);
+    }
+  };
+
   return (
     <Layout>
       <PageContent>
         <UserLibraryInfo
           totalSongs={songs.length}
-          searchOptions={songs.map((songEntry) => ({
-            id: `${songEntry.id}`,
-            value: songEntry.song.title,
-          }))}
+          searchOptions={getSearchOptionsFromSongs(songs)}
+          onToggleAlphabeticalSort={onSortSong}
         />
         <div style={{ height: '40px' }} />
         <SongsList songs={songs} />
