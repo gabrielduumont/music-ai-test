e65fd638f339d99377c840f38a5094591bcc8d1f
gabrielduumont <gabrielduumont@gmail.com>
2024-08-24 13:20:30 -0300

    Added missing colors and padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Layout from '@app/components/Layout';
+import UserLibrary from '@app/components/UserLibrary';
 
 export default function HomePage() {
   return (
     <Layout>
-      <div>HOME</div>
+      <div style={{ padding: '50px 145px' }}>
+        <UserLibrary />
+      </div>
     </Layout>
   );
 }
